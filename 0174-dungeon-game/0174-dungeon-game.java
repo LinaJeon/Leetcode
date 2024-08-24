a03be497a7674e1e538c5caa41cbfc1ed6a8c61f
LinaJeon <143653138+LinaJeon@users.noreply.github.com>
2024-08-24 16:27:19 +0900

    Time: 2 ms (72.21%), Space: 45.2 MB (42.83%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,33 +1,23 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        
-        // Create a DP table with the same dimensions as dungeon
-        int[][] dp = new int[m][n];
-        
-        // Fill the bottom-right corner
-        dp[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
-        
-        // Fill the last row (right to left)
-        for (int j = n - 2; j >= 0; j--) {
-            dp[m - 1][j] = Math.max(dp[m - 1][j + 1] - dungeon[m - 1][j], 1);
-        }
-        
-        // Fill the last column (bottom to top)
-        for (int i = m - 2; i >= 0; i--) {
-            dp[i][n - 1] = Math.max(dp[i + 1][n - 1] - dungeon[i][n - 1], 1);
-        }
-        
-        // Fill the rest of the DP table
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                int minHealthOnExit = Math.min(dp[i + 1][j], dp[i][j + 1]);
-                dp[i][j] = Math.max(minHealthOnExit - dungeon[i][j], 1);
+        int r = dungeon.length;
+        int c = dungeon[0].length;
+        int[][] dp = new int[r][c];
+
+        for (int i = r - 1; i >= 0; --i) {
+            for (int j = c - 1; j >= 0; --j) {
+                if (i == r - 1 && j == c - 1) { // Bottom-right corner
+                    dp[i][j] = Math.max(1, 1 - dungeon[i][j]);
+                } else if (i == r - 1) { // Last row
+                    dp[i][j] = Math.max(1, dp[i][j + 1] - dungeon[i][j]);
+                } else if (j == c - 1) { // Last column
+                    dp[i][j] = Math.max(1, dp[i + 1][j] - dungeon[i][j]);
+                } else { // Rest of the grid
+                    dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+                }
             }
         }
-        
-        // The result is the minimum initial health needed to start from (0, 0)
+
         return dp[0][0];
     }
 }
